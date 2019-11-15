cf0a9cdb32d04db7a7185285ca11fb8cc3cb8bc2
Sergio Junior <sergio.ltnj@gmail.com>
2019-11-14 23:00:19 -0300

    Adicionado no input da Data um calendario estilizado

--- src/main/resources/static/js/Cobranca.js
+++ src/main/resources/static/js/Cobranca.js
@@ -1,20 +1,22 @@
-$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
-	
-	var button = $(event.relatedTarget);
-	
-	
-	var codigoTitulo = button.data('codigo');
-	var descricaoTitulo = button.data('descricao');
-	
-	
-	var modal = $(this);
-	var form = modal.find('form');
-	var action = form.data('url-base');
-	if(!action.endsWith('/')){
-		action += '/';
-	}
-	form.attr('action', action + codigoTitulo);
-	
-	modal.find('.modal-body span').html('Voce deseja realmente excluir o titulo com a descrição: <br> <strong>' + descricaoTitulo + '</strong>?');
-	
-});
+$('#confirmacaoExclusaoModal').on(
+		'show.bs.modal',
+		function(event) {
+
+			var button = $(event.relatedTarget);
+
+			var codigoTitulo = button.data('codigo');
+			var descricaoTitulo = button.data('descricao');
+
+			var modal = $(this);
+			var form = modal.find('form');
+			var action = form.data('url-base');
+			if (!action.endsWith('/')) {
+				action += '/';
+			}
+			form.attr('action', action + codigoTitulo);
+
+			modal.find('.modal-body span').html(
+					'Voce deseja realmente excluir o titulo com a descrição: <br> <strong>'
+							+ descricaoTitulo + '</strong>?');
+
+		});
